8d9da22bea17a60ded0480a08cae4ab056fe2ec1
cxz <3125976329@qq.com>
2017-02-27 17:27:02 +0800

    0227

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+	include_once "lib/Mysql.class.php";
+	include_once "lib/Crawler.class.php";
+	include_once "lib/simple_html_dom.php";
+
+	$crawler = new crawler();
+	$html = $crawler->getHtml("http://www.baidu.com");
+
+	$dom = str_get_html($html);
+
+	$arrDom = $dom->find('a');
+
+	foreach ($arrDom as $key => $value) {
+		echo $value->href . '<br/>';
+	}
+
+	$dom->clear(); 
+	unset($dom);
