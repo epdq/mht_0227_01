3dacf93f939d58bea5134b4882d263479c992255
epdq <3125976329@qq.com>
2017-10-20 15:09:02 +0800

    1

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
-<?php 
-$a = [1, 2, 3];
-array_pop($a);
-var_dump($a);
+<?php
+    //连接本地的 Redis 服务
+   $redis = new Redis();
+   $redis->connect('127.0.0.1', 6379);
+   echo "Connection to server sucessfully";
+         //查看服务是否运行
+   echo "Server is running: " . $redis->ping();
+
+   $redis->set("tutorial-name", "Redis tutorial");
+   // 获取存储的数据并输出
+   echo "Stored string in redis:: " . $redis->get("tutorial-name");
